07de2d64c612aae9f829cc1ef8c1a7ccdeca2dcf
Mikhail Apsalikov <dee_jay@bk.ru>
2017-11-06 21:38:17 +0400

    Added empty unit test project

---
+++ Code/Hvs/Hvs.Entities/Account.cs
@@ -0,0 +1,11 @@
+﻿namespace Hvs.Entities
+{
+	using Interfaces.Architecture;
+    public class Account : IEntity
+    {
+	    public long Id { get; set; }
+	    public string Login { get; set; }
+	    public string Password { get; set; }
+	    public string Hash { get; set; }
+    }
+}
